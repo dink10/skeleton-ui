b2136e7a7b2b74a39d90df88e8203fedb0c61eec
Aleh Lipski <oleg.lipski@gismart.com>
2019-10-10 12:56:28 +0300

    Create index.js file for App folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'gismart-ui/core/style.css';
-import App from './App/App';
+import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
 // eslint-disable-next-line react/jsx-filename-extension
