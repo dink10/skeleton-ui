5eff96a7f37cc34a694199b62529d1a1af0b623c
Aleh Lipski <oleg.lipski@gismart.com>
2019-10-10 12:29:50 +0300

    Remove unused files and move font to library

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,90 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import Button from 'gismart-ui/core/components/Button';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-
-function Home() {
-  return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/topics">Topics</Link>
-        </li>
-        <Button>A</Button>
-      </ul>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Topic({ match }) {
-  return (
-    <div>
-      <h3>{match.params.topicId}</h3>
-    </div>
-  );
-}
-
-Topic.propTypes = {
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      topicId: PropTypes.string.isRequired,
-    }),
-  }).isRequired,
-};
-
-function Topics({ match }) {
-  return (
-    <div>
-      <h2>Topics</h2>
-      <ul>
-        <li>
-          <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-        </li>
-      </ul>
-
-      <Route path={`${match.path}/:topicId`} component={Topic} />
-      <Route
-        exact
-        path={match.path}
-        render={() => <h3>Please select a topic.</h3>}
-      />
-    </div>
-  );
-}
-
-Topics.propTypes = {
-  match: PropTypes.shape({
-    url: PropTypes.string.isRequired,
-    path: PropTypes.string.isRequired,
-  }).isRequired,
-};
+import { BrowserRouter as Router } from 'react-router-dom';
 
 export default function App() {
   return (
     <Router>
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
+      <h1>SCELETON</h1>
+      <Button> Test </Button>
     </Router>
   );
 }
--- src/App/App.stories.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable react/jsx-filename-extension */
-import React from 'react';
-import App from './App';
-
-export default { title: 'App' };
-
-export const DefaultApp = () => <App />;
