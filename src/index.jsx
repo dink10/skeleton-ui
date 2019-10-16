8ccfc5cd157b2118900fb62617e1dc2d4352a02b
Aleh Lipski <oleg.lipski@gismart.com>
2019-10-16 14:27:11 +0300

    Remove unused file

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-// import * as serviceWorker from './serviceWorker';
 // import { auth } from './services/authentication';
 // import { setConfig } from './actions/appActions';
 import store from './reducers/store';
@@ -21,8 +20,3 @@ render();
 // store.dispatch(setConfig())
 //   .then(auth)
 //   .then(() => render());
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
