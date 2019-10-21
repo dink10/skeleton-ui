f7c27048b1624a4391e8c7c80417fd7c24361ca2
Aleh Lipski <oleg.lipski@gismart.com>
2019-10-21 14:37:10 +0300

    Add basic Layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { ConnectedRouter } from 'connected-react-router'
 import { Switch, Route, Redirect } from 'react-router'
 import history from '/history'
+import Layout, { Header, Footer, Content } from 'gismart-ui/core/components/Layout'
 
 import { Login, NotFound } from './pages'
 import Secure from './Secure'
@@ -10,21 +11,25 @@ import Secure from './Secure'
 function App() {
   return (
     <ConnectedRouter history={history}>
-      <>
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route
-            path="/home"
-            render={() => (
-              <Secure>
-                <div>Hello home</div>
-              </Secure>
-            )}
-          />
-          <Route component={NotFound} />
-        </Switch>
-      </>
+      <Layout>
+        <Header>HEADER</Header>
+        <Content>
+          <Route exact path="/" render={() => <Redirect to="/home" />} />
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route
+              path="/home"
+              render={() => (
+                <Secure>
+                  <div>Hello home</div>
+                </Secure>
+              )}
+            />
+            <Route component={NotFound} />
+          </Switch>
+        </Content>
+        <Footer>©2019 Created by GISMART</Footer>
+      </Layout>
     </ConnectedRouter>
   )
 }
