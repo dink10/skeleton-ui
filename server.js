95e808aaf7e2cc6d5f73e6cd54d0185345677287
Aleh Lipski <oleg.lipski@gismart.com>
2019-11-19 16:34:13 +0300

    Change module responsible for proxy

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
 const Bundler = require('parcel-bundler')
 const express = require('express')
-const proxy = require('http-proxy-middleware')
+const proxy = require('express-http-proxy')
 
 const API_URL = process.env.API_URL
 const API_PREFIX = '/api'
 
 const app = express()
 
-const proxyRouter = (apiPrefix) => (req) => {
-  const pathname = req.url
-  return API_URL + pathname.replace(apiPrefix,'')
-}
-
-app.use(API_PREFIX, proxy({
-  target: API_URL,
-  router: proxyRouter(API_PREFIX),
-  changeOrigin: true,
-  logLevel: 'debug'
-}))
-
+app.use(API_PREFIX, proxy(API_URL))
 
 const bundler = new Bundler('./src/index.html')
 app.use(bundler.middleware())
