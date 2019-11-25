21c88012e00c0c6937ae6b4c7ced838d04227a32
Anton Aksenov <anton.aksenov@gismart.com>
2019-11-25 15:32:43 +0300

    add busy port check in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const Bundler = require('parcel-bundler')
 const express = require('express')
 const proxy = require('express-http-proxy')
 
-const API_URL = process.env.API_URL
+const { API_URL } = process.env
 const API_PREFIX = '/api'
 
 const app = express()
@@ -12,4 +12,15 @@ app.use(API_PREFIX, proxy(API_URL))
 const bundler = new Bundler('./src/index.html')
 app.use(bundler.middleware())
 
-app.listen(Number(process.env.PORT || 8080))
+function listen(port, tries = 0) {
+  app.listen(port, () => {
+    // eslint-disable-next-line
+	  console.log(`\nApp running on http://localhost:${port}\n`)
+  }).on('error', (error) => {
+    if (error.code === 'EADDRINUSE' && tries < 10) {
+      listen(port + 1, tries + 1)
+    }
+  })
+}
+
+listen(Number(process.env.PORT || 8080))
