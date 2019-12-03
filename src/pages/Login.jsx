c049d05cac8739b0e1d7f9eaa8701784e5ea079b
Aleh Lipski <oleg.lipski@gismart.com>
2019-12-03 15:59:28 +0300

    Fix infinity loop on log out action

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { goTo } from '/history'
 import { Button } from 'gismart-ui/core/components'
 import { loginAction } from '/actions/auth'
 import { PageWrapper } from './style'
 
-function Login({ login, isLoggedIn }) {
-  if (isLoggedIn) {
-    goTo('/')
-    return null
-  }
-
+function Login({ login }) {
   return (
     <PageWrapper>
       <Button size="large" onClick={login}>Login</Button>
@@ -20,16 +14,11 @@ function Login({ login, isLoggedIn }) {
 }
 
 Login.propTypes = {
-  isLoggedIn: PropTypes.bool.isRequired,
   login: PropTypes.func.isRequired,
 }
-
-const mapStateToProps = (state) => ({
-  isLoggedIn: state.auth.isLoggedIn,
-})
 
 const mapActionsToProps = (dispatch) => ({
   login: () => dispatch(loginAction()),
 })
 
-export default connect(mapStateToProps, mapActionsToProps)(Login)
+export default connect(null, mapActionsToProps)(Login)
